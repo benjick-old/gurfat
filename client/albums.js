ce10168602f80fb0c7d8edef76d882e1c1988ad0
Max Malm <benjick@dumfan.net>
2015-06-10 16:04:48 +0200

    more=more

--- client/albums.js
+++ client/albums.js
@@ -10,7 +10,6 @@ Template.albumsSingle.helpers({
 	first: function (self) {
 		var image = Images.findOne(self.files[0].id);
 		if(typeof(image) !== 'undefined') {
-			console.log(image)
 			image.s3 = image.copies.images.key.split('/').pop();
 			return image
 		}
